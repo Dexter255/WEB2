5018915fe6bf131e0a0b7b217c8e258807af88cb
Dexter <34897084+Dexter255@users.noreply.github.com>
2020-05-08 10:58:50 +0200

    [2.1] Prepravka
    Izbrisane komponente koje prikazuju listu vozila. Umesto liste vozila treba da se prikazuje lista Rent a car kompanija.

--- Project/src/app/app-routing.module.ts
+++ Project/src/app/app-routing.module.ts
@@ -1,9 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/content/home/home.component';
-import { RentACarComponent } from './components/content/rent-a-car/rent-a-car.component';
-import { VehicleDetailsComponent } from './components/content/rent-a-car/vehicle-details/vehicle-details.component';
-import { RentACarCompanyDetailsComponent } from './components/content/rent-a-car/rent-a-car-company-details/rent-a-car-company-details.component';
 import { FlightsComponent } from './components/content/flights/flights.component';
 import { FlightDetailsComponent } from './components/content/flights/flight-details/flight-details.component';
 import { AirlineCompanyDetailsComponent } from './components/content/flights/airline-company-details/airline-company-details.component';
@@ -35,10 +32,6 @@ const routes: Routes = [
       { path: ":id", component: FlightDetailsComponent },
       { path: ":id/:airline-companyName", component: AirlineCompanyDetailsComponent }
     ] },
-    { path: "rent-a-car", component: RentACarComponent, children: [
-      { path: ":id", component: VehicleDetailsComponent },
-      { path: ":id/:rac-companyName", component: RentACarCompanyDetailsComponent }
-    ]},
     { path: "login", component: LoginComponent },
     { path: "register", component: RegisterComponent },
     { path: "userProfile", component: ProfilKorisnikaComponent },
--- Project/src/app/app.module.ts
+++ Project/src/app/app.module.ts
@@ -8,11 +8,6 @@ import { HeaderComponent } from './components/header/header.component';
 import { ContentComponent } from './components/content/content.component';
 import { ProfilKorisnikaComponent } from './components/content/profil-korisnika/profil-korisnika.component';
 import { HomeComponent } from './components/content/home/home.component';
-import { RentACarComponent } from './components/content/rent-a-car/rent-a-car.component';
-import { VehicleListComponent } from './components/content/rent-a-car/vehicle-list/vehicle-list.component';
-import { VehicleDetailsComponent } from './components/content/rent-a-car/vehicle-details/vehicle-details.component';
-import { VehicleComponent } from './components/content/rent-a-car/vehicle-list/vehicle/vehicle.component';
-import { RentACarCompanyDetailsComponent } from './components/content/rent-a-car/rent-a-car-company-details/rent-a-car-company-details.component';
 import { FlightsComponent } from './components/content/flights/flights.component';
 import { FlightListComponent } from './components/content/flights/flight-list/flight-list.component';
 import { FlightDetailsComponent } from './components/content/flights/flight-details/flight-details.component';
@@ -27,7 +22,6 @@ import { ServerService } from './components/server.service';
 import { AdminPanelComponent } from './components/content/admin-panel/admin-panel.component';
 import { AdminHeaderComponent } from './components/content/admin-panel/admin-header/admin-header.component';
 import { AdminContentComponent } from './components/content/admin-panel/admin-content/admin-content.component';
-import { RentACarService } from './components/content/rent-a-car/rent-a-car.service';
 import { AdminsComponent } from './components/content/admin-panel/admin-content/admins/admins.component';
 import { AdminListComponent } from './components/content/admin-panel/admin-content/admins/admin-list/admin-list.component';
 import { AddAdminComponent } from './components/content/admin-panel/admin-content/admins/add-admin/add-admin.component';
@@ -61,11 +55,6 @@ import { AdminVehicleDetailsComponent } from './components/content/admin-panel/a
     PrijateljComponent,
     ListaPrijateljaComponent,
     HomeComponent,
-    RentACarComponent,
-    VehicleListComponent,
-    VehicleDetailsComponent,
-    VehicleComponent,
-    RentACarCompanyDetailsComponent,
     FlightsComponent,
     FlightListComponent,
     FlightDetailsComponent,
@@ -104,7 +93,7 @@ import { AdminVehicleDetailsComponent } from './components/content/admin-panel/a
     FormsModule,
     ReactiveFormsModule
   ],
-  providers: [ServerService, RacCompaniesService, AirlinesService, AdminsService, RentACarService],
+  providers: [ServerService, RacCompaniesService, AirlinesService, AdminsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
